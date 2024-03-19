00cee0a75ca50489d5bc96e3f46c187f73de7bca
bturcat <bturcat@gaming.tech>
2024-03-19 15:16:21 +0100

    c'est kré

--- PokeWorld/Source/GameObject/Creatures/CreatureStats.cs
+++ PokeWorld/Source/GameObject/Creatures/CreatureStats.cs
@@ -1,10 +1,19 @@
 ﻿public struct CreatureStats
 {
+    /*------------------------------------------------------------------------------------------*\
+    |                                                                                            |
+    |                                                                                            |
+    |                                          Fields                                            |
+    |                                                                                            |
+    |                                                                                            |
+    \*------------------------------------------------------------------------------------------*/
+
+    #region Fields
     int _hp;
     int _attack;
     int _magicpower;
     string _type;
     int _defense;
     int _mana;
-    
+    #endregion Fields
 }
