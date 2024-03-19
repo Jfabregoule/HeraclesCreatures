b4cc179cb145ccd813bc70cb1f038fe3ce00140b
jfabregoule <jfabregoule@gaming.tech>
2024-03-19 14:30:35 +0100

    Moves

---
+++ PokeWorld/Source/Creatures/Creatures.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeraclesCreatures.Source.Creatures
+{
+
+    #region Creatures Class
+
+    internal class Creatures
+    {
+        /*------------------------------------------------------------------------------------------*\
+        |                                                                                            |
+        |                                                                                            |
+        |                                          Fields                                            |
+        |                                                                                            |
+        |                                                                                            |
+        \*------------------------------------------------------------------------------------------*/
+
+        #region Fields
+
+        string _creatureName;
+        //List<Moves>   _Moves;
+        //List<Spells>  _Spells;
+        //HeroeStat     _Stats;
+
+        #endregion Fields
+
+        /*------------------------------------------------------------------------------------------*\
+        |                                                                                            |
+        |                                                                                            |
+        |                                        Properties                                          |
+        |                                                                                            |
+        |                                                                                            |
+        \*------------------------------------------------------------------------------------------*/
+
+        #region Properties
+
+        public string CreatureName { get => _creatureName; private set => _creatureName = value; }
+
+        #endregion Properties
+
+        /*------------------------------------------------------------------------------------------*\
+        |                                                                                            |
+        |                                                                                            |
+        |                                          Events                                            |
+        |                                                                                            |
+        |                                                                                            |
+        \*------------------------------------------------------------------------------------------*/
+
+        #region Events
+
+
+
+        #endregion Events
+
+        /*------------------------------------------------------------------------------------------*\
+        |                                                                                            |
+        |                                                                                            |
+        |                                         Methods                                            |
+        |                                                                                            |
+        |                                                                                            |
+        \*------------------------------------------------------------------------------------------*/
+
+        #region Methods
+        public Creatures() { }
+        public Creatures(string CreatureName) { }
+
+        #endregion Methods
+
+    }
+
+    #endregion Creatures Class
+
+}
---
+++ PokeWorld/Source/Creatures/Moves/AttackStats.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeraclesCreatures.Source.Creatures.Moves
+{
+    internal struct AttackStats
+    {
+
+        /*------------------------------------------------------------------------------------------*\
+       |                                                                                            |
+       |                                                                                            |
+       |                                          Fields                                            |
+       |                                                                                            |
+       |                                                                                            |
+       \*------------------------------------------------------------------------------------------*/
+
+        #region Fields
+
+        int     _power;
+        int     _accuracy;
+        int     _critRate;
+        int     _maxPP;
+        string  _type;
+
+        #endregion Fields
+
+        /*------------------------------------------------------------------------------------------*\
+        |                                                                                            |
+        |                                                                                            |
+        |                                        Properties                                          |
+        |                                                                                            |
+        |                                                                                            |
+        \*------------------------------------------------------------------------------------------*/
+
+        #region Properties
+
+        public int Power { get => _power; private set => _power = value; }
+
+        public int Accuracy { get => _accuracy; private set => _accuracy = value; }
+
+        public int CritRate { get => _critRate; private set => _critRate = value; }
+
+        public int MaxPP { get => _maxPP; private set => _maxPP = value; }
+
+        public string Type { get => _type; private set => _type = value; }
+
+        #endregion Properties
+
+        /*------------------------------------------------------------------------------------------*\
+        |                                                                                            |
+        |                                                                                            |
+        |                                          Events                                            |
+        |                                                                                            |
+        |                                                                                            |
+        \*------------------------------------------------------------------------------------------*/
+
+        #region Events
+
+
+
+        #endregion Events
+
+        /*------------------------------------------------------------------------------------------*\
+        |                                                                                            |
+        |                                                                                            |
+        |                                         Methods                                            |
+        |                                                                                            |
+        |                                                                                            |
+        \*------------------------------------------------------------------------------------------*/
+
+        #region Methods
+
+        public AttackStats() { }
+
+        #endregion Methods
+
+    }
+}
