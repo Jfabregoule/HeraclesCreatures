f2d7e9af8aba93295402290e1df1902d3c1a4264
bturcat <bturcat@gaming.tech>
2024-03-19 13:48:02 +0100

    gameobject et map object

---
+++ PokeWorld/MapObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeraclesCreatures
+{
+    internal class MapObject : GameObject
+    {
+        //Field
+        int _x;
+        int _y;
+
+        //Property
+        public int X { get { return _x; } set { _x = value; } }
+        public int Y { get { return _y; } set { _y = value; } }  
+
+        //Event
+
+        //Methods
+    }
+}
