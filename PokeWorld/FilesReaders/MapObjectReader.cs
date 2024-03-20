d204d732f11041e0af16934dad7d0e453e2fa3d1
Rayan Kotob <rayan.kotob22@gmail.com>
2024-03-20 12:31:51 +0100

    Slight adjustments to file reading

--- PokeWorld/FilesReaders/MapObjectReader.cs
+++ PokeWorld/FilesReaders/MapObjectReader.cs
@@ -64,13 +64,30 @@ namespace HeraclesCreatures.FilesReaders
 
         string[] ReadFile(string filePath)
         {
-            string solutionDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
-            string relativePath = $@"Map\MapObjects\{filePath}";
-            string mapObjectFilePath = Path.Combine(solutionDirectory, relativePath);
-            string[] mapObjectLines = File.ReadAllLines(mapObjectFilePath);
-            return mapObjectLines;
+            try
+            {
+                string solutionDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+                string relativePath = $@"{filePath}";
+                string mapObjectFilePath = Path.Combine(solutionDirectory, relativePath);
+                if (File.Exists(mapObjectFilePath))
+                {
+                    string[] mapObjectLines = File.ReadAllLines(mapObjectFilePath);
+                    return mapObjectLines;
+                }
+                else
+                {
+                    Console.WriteLine("File not found.");
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while reading the file: {ex.Message}");
+                return null;
+            }
         }
 
+        /*
         static object CreateInstanceFromString(string typeName, params object[] args)
         {
             Type type = Type.GetType(typeName);
@@ -80,6 +97,7 @@ namespace HeraclesCreatures.FilesReaders
             }
             return null;
         }
+        */
 
         bool GetWalkable(string[] mapObjectLines, int index)
         {
@@ -88,11 +106,11 @@ namespace HeraclesCreatures.FilesReaders
 
         char[,] GetDrawing(string[] mapObjectLines, int index)
         {
-            char[,] mapObjectDrawing = new char[5, 5];
+            char[,] mapObjectDrawing = new char[5, 10];
 
             for (int j = 0; j < 5; j++)
             {
-                for (int k = 0; k < 5; k++)
+                for (int k = 0; k < 10; k++)
                 {
                     mapObjectDrawing[j, k] = mapObjectLines[index + j + 1][k];
                 }
@@ -103,7 +121,7 @@ namespace HeraclesCreatures.FilesReaders
 
         ConsoleColor[,] GetColor(string[] mapObjectLines, int index)
         {
-            ConsoleColor[,] mapObjectColor = new ConsoleColor[5, 5];
+            ConsoleColor[,] mapObjectColor = new ConsoleColor[5, 10];
             string colorCode = "";
             int counter;
             for (int j = 0; j < 5; j++)
@@ -133,52 +151,60 @@ namespace HeraclesCreatures.FilesReaders
             CellData_ cellData = new CellData_();
             cellData.CellContent = new MapObject();
             string[] mapObjectLines = ReadFile(file);
-            
-            for (int i = 0; i < mapObjectLines.Length; i++)
+            if (mapObjectLines != null)
             {
-                /*
-                // Map Object
-                if (mapObjectLines[i] == "Type:")
+                for (int i = 0; i < mapObjectLines.Length; i++)
                 {
-                    object mapObject = CreateInstanceFromString(mapObjectLines[i + 1]);
-                    switch (mapObjectLines[i + 1]) {
-                        case "Chest":
-                            break;
-                        case "Ground":
-                            break;
-                        case "Wall":
-                            break;
+                    /*
+                    // Map Object
+                    if (mapObjectLines[i] == "Type:")
+                    {
+                        object mapObject = CreateInstanceFromString(mapObjectLines[i + 1]);
+                        switch (mapObjectLines[i + 1]) {
+                            case "Chest":
+                                break;
+                            case "Ground":
+                                break;
+                            case "Wall":
+                                break;
+                        }
+                        cellData.CellContent = mapObject;
                     }
-                    cellData.CellContent = mapObject;
-                }
-                */
+                    */
 
-                // Walkable
-                if (mapObjectLines[i] == "Walkable:")
-                {
-                    cellData.IsWalkable = GetWalkable(mapObjectLines, i);
+                    // Walkable
+                    if (mapObjectLines[i] == "Walkable:")
+                    {
+                        cellData.IsWalkable = GetWalkable(mapObjectLines, i);
+                    }
+
+                    // Drawing
+                    else if (mapObjectLines[i] == "Drawing:")
+                    {
+                        cellData.Drawing = GetDrawing(mapObjectLines, i);
+                    }
+
+                    // Foreground Color
+                    else if (mapObjectLines[i] == "FGColor:")
+                    {
+                        cellData.ForegroundColor = GetColor(mapObjectLines, i);
+                    }
+
+                    // Background Color
+                    else if (mapObjectLines[i] == "BGColor:")
+                    {
+                        cellData.BackgroundColor = GetColor(mapObjectLines, i);
+                    }
                 }
 
-                // Drawing
-                else if (mapObjectLines[i] == "Drawing:")
-                {
-                    cellData.Drawing = GetDrawing(mapObjectLines, i);
-                }
-
-                // Foreground Color
-                else if (mapObjectLines[i] == "FGColor:")
-                {
-                    cellData.ForegroundColor = GetColor(mapObjectLines, i);
-                }
-
-                // Background Color
-                else if (mapObjectLines[i] == "BGColor:")
-                {
-                    cellData.BackgroundColor = GetColor(mapObjectLines, i);
-                }
+                return cellData;
             }
-
-            return cellData;
+            else
+            {
+                Console.WriteLine("Error occurred while reading the file.");
+                return new CellData_();
+            }
+            
             
         }
 
--- PokeWorld/Program.cs
+++ PokeWorld/Program.cs
@@ -8,7 +8,7 @@ class Program
     static void Main()
     {
         MapObjectReader coubeh = new MapObjectReader();
-        CellData_ apagnan = coubeh.GetMapObjectCellData("Chest1.txt");
+        CellData_ apagnan = coubeh.GetMapObjectCellData("Ressources/MapObjects/Chest1.txt");
         apagnan.printCellData();
 
     }
