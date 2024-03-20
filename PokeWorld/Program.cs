36a3fd7f939f6fbd5e3e4607da6640ab0220a967
Jfabregoule <Jfabregoule@gaming.tech>
2024-03-20 17:46:30 +0100

    Start of Creatures and Moves Parsing

--- PokeWorld/Program.cs
+++ PokeWorld/Program.cs
@@ -8,18 +8,6 @@ class Program
     public static void Main(string[] args)
     {
         GameManager gameManager = new GameManager();
-        Player Hercule = new Player();
-        Creatures Tiger = new Creatures("Tiger");
-        Potion Popo = new Potion();
-
-        Hercule.AddCreature(Tiger);
-        Hercule.AddItems(Popo);
-
-        Console.WriteLine(Hercule.Creatures[0].Stats.health);
-        Hercule.Creatures[0].TakeDamage(30);
-        Console.WriteLine(Hercule.Creatures[0].Stats.health);
-        Hercule.Items[0].Use(ref Tiger);
-        Console.WriteLine(Hercule.Creatures[0].Stats.health);
 
         gameManager.InitializeGame();
         gameManager.GameLoop();
--- PokeWorld/Source/GameManager.cs
+++ PokeWorld/Source/GameManager.cs
@@ -1,4 +1,7 @@
-﻿using HeraclesCreatures.Source.Map;
+﻿using HeraclesCreatures.GameObject;
+using HeraclesCreatures.Source.GameObject.Creatures;
+using HeraclesCreatures.Source.GameObject.Items;
+using HeraclesCreatures.Source.Map;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -22,9 +25,10 @@ namespace HeraclesCreatures.Source
 
         #region Fields
 
-        InputManager    _inputManager;
-        MapClass        _map;
-        bool            _isRunning;
+        InputManager                        _inputManager;
+        MapClass                            _map;
+        Dictionary<string, CreatureStats>   _creaturesStats;
+        bool                                _isRunning;
 
         #endregion Fields
 
@@ -68,6 +72,20 @@ namespace HeraclesCreatures.Source
         {
             _isRunning = true;
             _inputManager = new InputManager();
+
+            Player Hercule = new Player();
+            Creatures Tiger = new Creatures("Tiger");
+            Potion Popo = new Potion();
+
+            Hercule.AddCreature(Tiger);
+            Hercule.AddItems(Popo);
+
+            Console.WriteLine(Hercule.Creatures[0].Stats.health);
+            Hercule.Creatures[0].TakeDamage(30);
+            Console.WriteLine(Hercule.Creatures[0].Stats.health);
+            Hercule.Items[0].Use(ref Tiger);
+            Console.WriteLine(Hercule.Creatures[0].Stats.health);
+
         }
         public void GameLoop()
         {
@@ -80,6 +98,16 @@ namespace HeraclesCreatures.Source
                 }
             }
 
+        }
+
+        private void GetCreaturesStats() 
+        {
+
+        }
+
+        private void GetMoves() 
+        {
+            
         }
 
         #endregion Methods
