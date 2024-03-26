1de80ec028f3361c040cb6e261e5456685d80cb9
Rayan El Kotob <rayan.kotob22@gmail.com>
2024-03-26 22:30:31 +0100

    hhhhh

--- HeraclesCreatures/Program.cs
+++ HeraclesCreatures/Program.cs
@@ -8,26 +8,8 @@ namespace HeraclesCreatures
     {
         public static void Main(string[] args)
         {
-
-            //FileManager shoukran = new FileManager();
-            //shoukran.FillAllDictionnaries();
-            //Dictionary<string, Scene> scenes = shoukran.ScenesData;
-            //scenes["FirstScene"].DisplayScene();
-            
-
             GameManager gameManager = new GameManager();
-            
-            Console.WriteLine();
-           
-            //static void DisplayDictionary<T>(Dictionary<string, T> dictionary)
-            //{
-            //    Console.WriteLine($"Dictionary<{typeof(string).Name}, {typeof(T).Name}>:");
-            //    foreach (var kvp in dictionary)
-            //    {
-            //        Console.WriteLine($"Key: {kvp.Key}, Value: {kvp.Value}");
-            //    }
-            //    Console.WriteLine();
-            //}
+
             gameManager.GameLoop();
 
         }
